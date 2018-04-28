9a8c1c5003a0902d72bbe111eca7693412aaca61
vilden <419957563@qq.com>
2018-04-28 13:50:53 +0800

    learn node

--- node.js/charter 2/编程模式.js
+++ node.js/charter 2/编程模式.js
@@ -7,24 +7,3 @@ console.log("I'm waiting for all my work finish.")
 
 
 
-
-var fs = require("fs")
-fs.open(
-    'info.txt',
-    'r',
-    function (err, handle) {
-        var buf = new Buffer(100000);
-        fs.read(
-            handle,
-            buf,
-            0,
-            100000,
-            null,
-            function (err, length) {
-                console.log(buf.toString('utf-8', 0, length));
-                file.close(file, function () {})
-            }
-
-        )
-    }
-)
